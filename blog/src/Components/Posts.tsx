207ec8325adb0dd237f05689909adbaa43450926
Guibrick <guido.bertaina@appliedtechnology.se>
2023-01-17 01:20:01 +0100

    Display list of cards.

--- blog/src/Components/Posts.tsx
+++ blog/src/Components/Posts.tsx
@@ -1,4 +1,10 @@
 import { useState, useEffect } from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 type resultProps = {
     id: number;
@@ -17,25 +23,45 @@ function Posts() {
             });
             const jsonData = await data.json();
 
+            console.log(result)
+
             setResult(jsonData.posts)
         }
         getData();
     }, []);
 
+    const mapping = result.map((value) => { return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {value.title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {value.tags}
+                </Typography>
+                <Typography variant="body2">
+                    {value.body}
+                    <br />
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>)
+    });
+
     return (
-        <div>
-            {result.map((value) => {
-                return (
-                    <div>
-                        <div>{value.id}</div>
-                        <div>{value.title}</div>
-                        <div>{value.body}</div>
-                        <div>{value.tags}</div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+        <>
+            <Box
+                component="span"
+                sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+            >
+                <div>
+                    {mapping}
+                </div>
+            </Box>
+        </>
+    )
 }
 
 export default Posts;
